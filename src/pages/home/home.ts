7d2a05b8cde920e22f311c534cd03101d446dc18
smoquet <sr_moquette@yahoo.com>
2016-11-22 16:12:46 +0100

    writes STATUS and gets OK

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,9 +15,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
   document.addEventListener("deviceready", onDeviceReady, false);
-  function onDeviceReady() {
-    alert("DEvice Ready")
-  }
+  function onDeviceReady() {alert("DEvice Ready")};
+
+
   }
 
   // int = getEC();
@@ -28,13 +28,33 @@ export class HomePage {
   }
 
   requestPermission() {
+    var str = '';
     serial.requestPermission({vid: 9025, pid: 32845, driver: 'CdcAcmSerialDriver'},
+    // if permission is granted
     function success(){
-      // BaudRate for our Chip: 115200, Adruino: 9600
-      var opts = {"baudRate":9600, "dataBits":8, "stopBits":1, "parity":0, "dtr":false}
+      var opts = {"baudRate":9600, "dataBits":8, "stopBits":1, "parity":0, "dtr":true, 'rts':true}
+      //  open serial port
       serial.open(opts,
+        // if port is succsefully opened
         function success(){
-          alert("Success");
+          alert("Port Succesfully Opened");
+          // register the read callback
+           serial.registerReadCallback(
+             function success(data){
+              //  print data to screen
+              var view = new Uint8Array(data);
+              var s = String.fromCharCode.apply(String,view);
+              str += s;
+              if (s.endsWith('\n')){
+                alert(str);
+                str = '';
+              }
+            },
+          // error attaching the callback
+            function error(evt){
+              alert(evt);
+            }
+           );
         }, function error(evt){
           alert(evt);
         }
@@ -42,9 +62,21 @@ export class HomePage {
     },
     function error(evt){
       alert(evt);
-    }
-  );
+    },
+    );
 }
+    getEC() {
+      // ask for the temperature and EC
+      // serial.write('STATUS',function success(), function error());
+      serial.write("STATUS\r\n",
+        function success(){alert('Succesfully written something')},
+        function error(status){alert('Failed to write'+status)});
+
+      // serial.read(
+      //   function success(buffer){alert('Succesfully reading')},
+      //   function error(){'Failed reading'});
+    }
+
 }
 
 
