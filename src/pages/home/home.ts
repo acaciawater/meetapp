2411ee584250af1520d1c5ca4fec26dc7ff57096
smoquet <sr_moquette@yahoo.com>
2016-11-24 16:56:01 +0100

    adds saving functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { File } from 'ionic-native';
 
 declare var serial;
+declare var cordova: any;
+
 
 var ec_value = '100';
 var is_logging = false;
+
+var base_path:string = cordova.file.dataDirectory;
+var dir_name = 'AcaciaData';
+var file_name = 'measurement_table.csv';
+var data = 'blabla';
+var path = base_path+dir_name;
+
+
 
 class Records {
   ec: string;
@@ -125,64 +135,50 @@ export class HomePage {
       }
     }
 
+    writeNewFile(path, file_name, data){
+      alert('init writeNewFile');
+      // File.writeFile(path, file_name, data, false).then(_ => alert('writeFile success path = '+path+' file_name = '+file_name+' data = '+data)).catch(err => alert('writeFile '+JSON.stringify(err)+ ' path = '+path+' file_name = '+file_name+' data = '+data));
+      File.createFile(path, file_name, false).then(_ => alert('createFile success path = '+path+' file_name = '+file_name)).catch(err => alert('createFile '+JSON.stringify(err)));
+    }
 
+    newDirAndFile(base_path, dir_name, path, file_name, data){
+      alert('init newDirAndFile');
+      File.createDir(base_path, dir_name, false).then(_ => this.writeNewFile(path, file_name, data)).catch(errCD => alert('createDir '+base_path+dir_name+' '+JSON.stringify(errCD)));
+    }
+
+    writeExistingFile(path, file_name, data){
+      alert('init writeExistingFile');
+      File.writeExistingFile(path, file_name, data).then(_ => alert('writeExistingFile success')).catch(err => alert('writeExistingFile '+JSON.stringify(err)));
+    }
+
+    writeNewOrExistingFile(path, file_name, data){
+      alert('writeNewOrExistingFile');
+      File.checkFile(path+'/', file_name).then(_ => this.writeExistingFile(path, file_name, data)).catch(err => this.writeNewFile(path, file_name, data));
+    }
+      
     saveMeasurement(){
-
+      alert('init saveMeasurement');
+      var base_path:string = cordova.file.dataDirectory;
+      var dir_name = 'AcaciaData';
+      var file_name = 'measurement_table.csv';
+      var data = 'blabla';
+      var path = base_path+dir_name;
+      File.checkDir(base_path, dir_name).then(_ => this.writeNewOrExistingFile(path, file_name, data)).catch(err => this.newDirAndFile(base_path, dir_name, path, file_name, data));
+    }
+    checkDir(){
+      var base_path:string = cordova.file.dataDirectory;
+      var dir_name = 'AcaciaData';
+      var file_name = 'measurement_table.csv';
+      var data = 'blabla';
+      var path = base_path+dir_name;
+      File.checkDir(base_path, dir_name).then(_ => alert(base_path+dir_name+' is found')).catch(err => alert('checkDir '+JSON.stringify(err)));
+    }
+    checkFile(){
+      var base_path:string = cordova.file.dataDirectory;
+      var dir_name = 'AcaciaData';
+      var file_name = 'measurement_table.csv';
+      var data = 'blabla';
+      var path = base_path+dir_name;
+      File.checkFile(path+'/', file_name).then(_ => alert('checkFile '+path+file_name+' found')).catch(err => alert('checkFile '+path+'/'+file_name+' error ='+JSON.stringify(err)));
     }
 }
-
-
-
-
-
-
-//   requestPermission() {
-//     ec_value = '';
-//     serial.requestPermission({vid: 9025, pid: 32845, driver: 'CdcAcmSerialDriver'},
-//     // if permission is granted
-//     function success(){
-//       var opts = {"baudRate":9600, "dataBits":8, "stopBits":1, "parity":0, "dtr":true, 'rts':true}
-//       //  open serial port
-//       serial.open(opts,
-//         // if port is succsefully opened
-//         function success(){
-//           alert("Port Succesfully Opened");
-//           // register the read callback
-//            serial.registerReadCallback(
-//              function success(data){
-//               // 1) er is data binnen gekomen
-//               // 2) verwerk deze data
-//               var view = new Uint8Array(data);
-//               var s = String.fromCharCode.apply(String,view);
-//               ec_value += s;
-//               // 3) alert deze data
-//               alert(ec_value);
-//               // 4) verwijder data
-//               ec_value = '';
-//               // 5) als record aan i, verstuur nog een 'R'
-//               serial.write("R\r\n");
-//               if (s.endsWith('\n')){
-//                 displayValue(ec_value);
-//                 if (is_logging){
-//                   serial.write("R\r\n")
-//                 }
-//                 // heb ik niet op stop gedrukt stuur dan nog een 'R'
-// //                alert(this.value)
-//                 ec_value = '';
-//               }
-//             },
-//           // error attaching the callback
-//             function error(evt){
-//               alert(evt);
-//             }
-//            );
-//         }, function error(evt){
-//           alert(evt);
-//         }
-//       );
-//     },
-//     function error(evt){
-//       alert(evt);
-//     },
-//     );
-// }
